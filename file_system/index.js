59aa0d5c69ae285c1607ef864bb29484f38a1300
Pablo Santos <iphonepablosantos@gmail.com>
2025-01-12 21:02:36 +0000

    api

--- file_system/index.js
+++ file_system/index.js
@@ -1,9 +1,10 @@
-let {readFile} = require('fs');
+let express = require('express');
+let app = express();
 
-readFile("arquivo.txt", "utf8", (error, texto) => {
-    if(error) {
-        throw error;
-    } else {
-        console.log(texto);
-    }
+app.get('/', function(req, res) {
+    res.send('Primeira rota com Express');
+});
+
+app.listen(3000, function() {
+    console.log("A aplicação está funcionando na porta 3000");
 });
